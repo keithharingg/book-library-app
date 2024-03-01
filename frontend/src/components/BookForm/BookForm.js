f67242d6ea344a0c7bd27244e8f3c680b8d425ae
Taras Nesterenko <tnestere@gmail.com>
2024-03-01 16:34:50 +0100

    Initialize Redux and display Books

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import './BookForm.css';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/actionCreators';
 
 const BookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && author) {
+      const book = {
+        title,
+        author,
+      };
       // dispatch action
-      console.log(title, author);
+      dispatch(addBook(book));
       setTitle('');
       setAuthor('');
     }
--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,9 +1,25 @@
+import { useSelector } from 'react-redux';
 import './BookList.css';
 
 const BookList = () => {
+  const books = useSelector((state) => state.books); // hook useSelector subscribes on chunk of needed state (books in this variant)
+
   return (
     <div className="app-block book-list">
-      <h2>book list</h2>
+      <h2>Books List</h2>
+      {books.length === 0 ? (
+        <p>No books have been added yet</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={i}>
+              <div>
+                {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
